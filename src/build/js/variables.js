3801516f0bde58ea1d69b86becc7ae6066641fd6
Patricia Rozada <patricia.rozada@splashthat.com>
2022-10-30 23:42:08 +0100

    Update style-dict config and generate variables

---
+++ src/build/js/variables.js
@@ -0,0 +1,33 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 30 Oct 2022 22:15:00 GMT
+ */
+
+export const ColorBaseGrayLight = "#cccccc";
+export const ColorBaseGrayMedium = "#999999";
+export const ColorBaseGrayDark = "#111111";
+export const ColorBaseRed = "#ff0000";
+export const ColorBaseGreen = "#00ff00";
+export const ColorFontBase = "#ff0000";
+export const ColorFontSecondary = "#00ff00";
+export const ColorFontTertiary = "#cccccc";
+export const SizeFontSmall = "0.75rem"; // the small size of the font
+export const SizeFontMedium = "1rem"; // the medium size of the font
+export const SizeFontLarge = "2rem"; // the large size of the font
+export const SizeFontBase = "1rem"; // the base size of the font
+export const GlobalBase = "80";
+export const GlobalSm = "80";
+export const GlobalMd = "80 * 2";
+export const GlobalLg = "80 * 3";
+export const GlobalXl = "80*4";
+export const GlobalXs = "80 / 2";
+export const GlobalDanger4 = "#931F1D";
+export const GlobalDanger5 = "#551211";
+export const GlobalDanger3 = "#D73533";
+export const GlobalDanger2 = "#E78988";
+export const GlobalDanger1 = "#FCEEEE";
+export const GlobalPrimary1 = "#FED7F4";
+export const GlobalPrimary2 = "#FE9AE5";
+export const GlobalPrimary3 = "#820263";
+export const GlobalPrimary4 = "#50013D";
+export const GlobalPrimary5 = "#28011E";
