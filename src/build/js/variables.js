17cf70872a0831dd1e4abdfa848241278861199a
Patricia Rozada <patricia.rozada@splashthat.com>
2022-11-01 16:49:24 +0100

    Update color vars and sizes

--- src/build/js/variables.js
+++ src/build/js/variables.js
@@ -1,26 +1,14 @@
 /**
  * Do not edit directly
- * Generated on Sun, 30 Oct 2022 22:15:00 GMT
+ * Generated on Tue, 01 Nov 2022 15:45:51 GMT
  */
 
-export const ColorBaseGrayLight = "#cccccc";
-export const ColorBaseGrayMedium = "#999999";
-export const ColorBaseGrayDark = "#111111";
-export const ColorBaseRed = "#ff0000";
-export const ColorBaseGreen = "#00ff00";
-export const ColorFontBase = "#ff0000";
-export const ColorFontSecondary = "#00ff00";
-export const ColorFontTertiary = "#cccccc";
-export const SizeFontSmall = "0.75rem"; // the small size of the font
-export const SizeFontMedium = "1rem"; // the medium size of the font
-export const SizeFontLarge = "2rem"; // the large size of the font
-export const SizeFontBase = "1rem"; // the base size of the font
 export const GlobalBase = "80";
-export const GlobalSm = "80";
-export const GlobalMd = "80 * 2";
-export const GlobalLg = "80 * 3";
-export const GlobalXl = "80*4";
-export const GlobalXs = "80 / 2";
+export const GlobalSm = "80px";
+export const GlobalMd = "160px";
+export const GlobalLg = "240px";
+export const GlobalXl = "420px";
+export const GlobalXs = "40px";
 export const GlobalDanger4 = "#931F1D";
 export const GlobalDanger5 = "#551211";
 export const GlobalDanger3 = "#D73533";
@@ -30,4 +18,20 @@ export const GlobalPrimary1 = "#FED7F4";
 export const GlobalPrimary2 = "#FE9AE5";
 export const GlobalPrimary3 = "#820263";
 export const GlobalPrimary4 = "#50013D";
-export const GlobalPrimary5 = "#28011E";
+export const GlobalPrimary5 = "#28001F";
+export const GlobalWhite = "#fff";
+export const GlobalSecondary3 = "#FF5400";
+export const GlobalSecondary4 = "#7A2900";
+export const GlobalSecondary5 = "#3D1400";
+export const GlobalSecondary2 = "#FFC6AD";
+export const GlobalSecondary1 = "#FFF1EB";
+export const GlobalSuccess5 = "#121A0F";
+export const GlobalSuccess4 = "#2D4125";
+export const GlobalSuccess3 = "#5A824A";
+export const GlobalSuccess2 = "#C7DBBD";
+export const GlobalSuccess1 = "#F4F8F2";
+export const GlobalNeutral1 = "#F4F4F6";
+export const GlobalNeutral2 = "#C7C7D1";
+export const GlobalNeutral3 = "#84849A";
+export const GlobalNeutral4 = "#49495A";
+export const GlobalNeutral5 = "#1C1C22";
