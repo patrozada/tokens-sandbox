9b9d30752927dd9ae4852cc1cda369ac5d430e63
Patricia Rozada <patricia.rozada@splashthat.com>
2022-11-02 13:21:40 +0100

    Update tokens and variables

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import * as DT  from '../build/js/variables';
 
 const Container = styled.div`
-    background-color: ${DT.GlobalSecondary1};
+    background-color: ${DT.GlobalColorPrimary1};
     min-height: 100vh;
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
-    gap: ${DT.GlobalSm};
-    padding: ${DT.GlobalSm} ${DT.GlobalXs}
+    gap: ${DT.GlobalSizingSm};
+    padding: ${DT.GlobalSizingSm} ${DT.GlobalSizingXs}
 `
 
 export default function Layout({children}) {
